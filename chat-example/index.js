5d1ef0ab7e0c4e4b9dff2b783c1137c5fa5686f9
Mark Milutinovic <mmilutinovic1313@gmail.com>
2017-04-06 15:44:39 -0400

    Support for author = username

--- chat-example/index.js
+++ chat-example/index.js
@@ -3,13 +3,62 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Chatroom
+
+var numUsers = 0;
+
 io.on('connection', function(socket){
+var addedUser = false;
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
 
